93e107b45ba79d82b1d5e38b00d9231fa6c9ec73
RachWei <rachaelwei@gmail.com>
2024-05-01 16:44:28 -0400

    Added videos

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -6,6 +6,9 @@ import styled from 'styled-components'
 import stringgirl from '../images/string_girl.png'
 import mailbox from '../images/mailbox.png'
 import architect from '../images/architect.jpeg'
+import binary from '../images/1010.mp4'
+import memory from '../images/memory.mp4'
+// import quiet from '../images/quiet.JPG'
 
 const ArtPage = styled.div`
     padding: 2em;
@@ -14,9 +17,14 @@ const ArtPage = styled.div`
     width: 100%;
 `
 
+const Video = styled.video`
+    height: 30vh;
+`
+
 function DraggableImage(props) {
     const width = props.width
     const height = props.height
+    const title = props.title
 
     return (
         <Draggable>
@@ -36,9 +44,18 @@ function DraggableImage(props) {
 function Art() {
     return (
         <ArtPage>
-            <DraggableImage width={100} height={260} background={stringgirl}/>
+            <Video playsInline loop muted autoPlay alt="All the devices">
+                <source src={binary} type="video/mp4" />
+                Your browser does not support the video tag.
+            </Video>
+            <Video playsInline loop muted autoPlay alt="All the devices">
+                <source src={memory} type="video/mp4" />
+                Your browser does not support the video tag.
+            </Video>
+            <DraggableImage width={100} height={260} background={stringgirl} title="string girl"/>
             <DraggableImage width={100} height={260} background={mailbox}/>
             <DraggableImage width={100} height={260} background={architect}/>
+            {/* <DraggableImage width={100} height={260} background={quiet}/> */}
             <Draggable><h1>I'm draggable!</h1></Draggable>
         </ArtPage>
     );
